569aa0b066ffd447b9bba994b13b3645ea0a6048
Mooaz Sayyed <142921993+mooazsayyed@users.noreply.github.com>
2024-03-27 12:19:47 +0530

    Add files via upload

---
+++ client/src/components/ServicesElectrician.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ServicesElectrician from './ServicesElectrician';
+
+const Electrician = () => {
+    return (
+        <div className="electrician">
+            <ServicesElectrician />
+        </div>
+    );
+};
+
+export default Electrician;
